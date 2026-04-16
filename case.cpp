91fd6329c62d204bc25118c6f03ac1349ff62847
lapinouche <ammiratiflorent5406@gmail.com>
2026-04-16 15:59:42 +0200

    Update case.cpp

--- case.cpp
+++ case.cpp
@@ -1,12 +1,30 @@
-#include <iostream> 
+#include <iostream>
 #include <vector>
+#include <array>
+#include <map>
+#include <cstdlib> // for random
+
+using namespace std;
 // Classe abstraite de base
-class Case {
-public :
 
-    virtual char afficher () = 0;
-    virtual ~Case () = default ;
+class CaseFactory{
+    public:
+        CaseFactory(){}
 
+        void creerCase(const Case& TypeCase){
+            TypeCase NewCase();
+        }
+};
+
+class Case{
+    private:
+        bool visiter;
+
+    public:
+        Case(const bool& visiter=false) : visiter(visiter) {}
+
+        virtual char afficher() = 0;
+        virtual ~Case() = default;
 };
 
 class Mur : public Case {
@@ -81,3 +99,95 @@ public :
     }
 
 };
+
+
+class Donjon {
+    private:
+        vector<vector<Case*>> grille;
+
+    public:
+        Donjon(const vector<vector<Case*>>& grille) : grille(grille) {}
+
+        void generer(int largeur, int hauteur);
+
+        void afficher();
+
+        vector<pair<int, int>> trouverChemin();
+
+        void genererLabyrinthe(vector<vector<Case*>> grille, int x, int y){
+            grille[x][y].visiter = true;
+            vector<string> directions = {"NORD", "SUD", "EST", "OUEST"};
+
+            for (string d : directions){
+                if (d == "NORD"){
+                    int nx = x;
+                    int ny = y + 1;
+                }
+                if (d == "SUD"){
+                    int nx = x;
+                    int ny = y - 1;
+                }
+                if (d == "EST"){
+                    int nx = x + 1;
+                    int ny = y;
+                }
+                if (d == "OUEST"){
+                    int nx = x - 1;
+                    int ny = y;
+                }
+                (l, h) = grille.size()
+                if (){
+                    // casser le mur entre (x, y) et (nx, ny)
+                    genererLabyrinthe(grille, nx, ny);
+                }
+
+
+            }
+
+        }
+
+        Case poserEntree(const vector<vector<Case*>>& grille){
+            Case entree = grille[0][0];
+            return entree;
+        }
+
+        void poserSortie(const vector<vector<Case*>>& grille){
+            Case sortie = grille[20][20];
+            return sortie;
+        }
+
+        void initialiserGrille(int largeur, int hauteur){
+            for (int i=0; i<largeur; i++){
+                for (int j=0; j<hauteur; j++){
+                    grille[i][j] = CaseFactory::creeCase(TypeCase::MUR)
+                }
+            }
+            genererLabyrinthe(grille, 1, 1);
+            poserEntree(grille);
+            poserSortie(grille);
+        }
+
+        void placerElement(const vector<vector<Case*>>& grille){
+            for (int i=0; i<20; i++){
+                for (int j=0; j<20; j++){
+                    if (grille[i][j] == PASSAGE){
+                        int r = rand() % 100;
+                        if (r < 5){
+                            grille[i][j] = CaseFactory::creerCase(TRESOR);
+                        }
+                        else if (r < 10) {
+                            grille[i][j] = CaseFactory::creerCase(MONSTRE);
+                        }
+                        else if (r < 13) {
+                            grille[i][j] = CaseFactory::creerCase(PIEGE);
+                        }
+                    }
+                }
+            }
+        }
+};
+
+
+int main(){
+    return 0;
+}

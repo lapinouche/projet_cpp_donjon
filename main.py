a85b92c48538d4e1a35f1b4792286f99d08c7b8c
lapinouche <ammiratiflorent5406@gmail.com>
2026-04-19 21:10:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,23 +19,26 @@ enum class TypeCase {
 };
 
 class CaseFactory {
-public:
-    static Case* creerCase(TypeCase type) {
-        if (type == TypeCase::MUR) {
-            return new Mur();
-        } else if (type == TypeCase::PASSAGE) {
-            return new Passage();
-        } else if (type == TypeCase::TRESOR) {
-            return new Tresor();
-        } else if (type == TypeCase::MONSTRE) {
-            return new Monstre();
-        } else if (type == TypeCase::PIEGE) {
-            return new Piege();
-        } else {
-            //throw std::invalid_argument("Type de case inconnu");
-            return nullptr;
+    public:
+        static Case* creerCase(TypeCase type) {
+            switch(type){
+                case TypeCase::MUR:{
+                    return new Mur();
+                }
+                case TypeCase::PASSAGE:{
+                    return new Passage();
+                }
+                case TypeCase::TRESOR:{
+                    return new Tresor();
+                }
+                case TypeCase::PIEGE:{
+                    return new Piege();
+                }
+                default:{
+                    return nullptr;
+                }
+            }
         }
-    }
 };
 
 class Case{
